87527eb4f1fef11e9dfd10b657bce95c6385b1a3
shubh batra <godwithsam@gmail.com>
2023-05-29 17:31:13 +0100

    Made some changes.

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -1,17 +1,14 @@
-import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
-
+import { connectToDB } from "@utils/database";
 
 export const GET = async (request, { params }) => {
     try {
-        await connectToDB();
+        await connectToDB()
 
-        const prompts = await Prompt.find({
-            creator: params.id
-        }).populate('creator');
+        const prompts = await Prompt.find({ creator: params.id }).populate("creator")
 
         return new Response(JSON.stringify(prompts), { status: 200 })
     } catch (error) {
-        return new Response("Failed to fetch all posts", { status: 500 })
+        return new Response("Failed to fetch prompts created by user", { status: 500 })
     }
-}
+} 
